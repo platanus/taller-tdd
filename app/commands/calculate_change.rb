bb7b822673b1b4a79174767fceeef6b5eb2707ae
Juan Ignacio Donoso <jidonoso@gmail.com>
2018-11-09 17:29:06 -0300

    wip

--- app/commands/calculate_change.rb
+++ app/commands/calculate_change.rb
@@ -1,10 +1,15 @@
 class CalculateChange < PowerTypes::Command.new(:amount)
+  UNITS = [50,10,5,1]
+
   def perform
     coins = []
 
-    coins += [10] * (@amount / 10) if @amount >= 10
-    coins << 5 if (@amount - coins.sum) >= 5
-    coins << [1] * (@amount - coins.sum) if (@amount - coins.sum).positive?
+    coins = UNITS.map do |unit|
+      coins += [unit] * ((@amount - coins.sum) / unit) if @amount >= unit
+      # coins += [10] * ((@amount - coins.sum) / 10) if @amount >= 10
+      # coins += [5] * ((@amount - coins.sum) / 5) if @amount  >= 5
+      # coins += [1] * ((@amount - coins.sum) / 1) if @amount >= 1
+    end
 
     coins.flatten.sort
   end
